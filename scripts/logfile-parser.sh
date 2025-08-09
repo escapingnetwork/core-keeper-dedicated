961c5f2ffafc6e4bff337e68ef8a2a3d8da574bb
PhasecoreX <phasecorex@gmail.com>
2025-08-09 15:12:41 +0000

    Fix logfile-parser.sh
    If any of DISCORD_PLAYER_JOIN_ENABLED, DISCORD_PLAYER_LEAVE_ENABLED, or DISCORD_SERVER_START_ENABLED were set to false, logfile-parser.sh would stop running when receiving any of those events.

--- scripts/logfile-parser.sh
+++ scripts/logfile-parser.sh
@@ -6,7 +6,7 @@ LogParser() {
     while IFS= read -r line; do
         echo "$line"
 
-        if [[ "$line" == "[userid:"*"] player"*"connected islocalplayer="* ]]; then
+        if [[ "${DISCORD_PLAYER_JOIN_ENABLED,,}" == true ]] && [[ "$line" == "[userid:"*"] player"*"connected islocalplayer="* ]]; then
 
             # Extract the steamid and character name
             steamid=$(echo "$line" | awk -F'[[]|[ :]+|[]]' '{print $3}')
@@ -16,15 +16,12 @@ LogParser() {
             # Store character name for future use
             characters[$steamid]=$char_name
 
-            [[ "${DISCORD_PLAYER_JOIN_ENABLED,,}" == false ]] && return 0
-
             # Build message from vars and send message
             message=$(char_name="$char_name" steamid="$steamid" envsubst <<<"$DISCORD_PLAYER_JOIN_MESSAGE")
             SendDiscordMessage "$DISCORD_PLAYER_JOIN_TITLE" "$message" "$DISCORD_PLAYER_JOIN_COLOR"
         fi
 
-        if [[ "$line" == *"Disconnected from userid:"* ]]; then
-            [[ "${DISCORD_PLAYER_LEAVE_ENABLED,,}" == false ]] && return 0
+        if [[ "${DISCORD_PLAYER_LEAVE_ENABLED,,}" == true ]] && [[ "$line" == *"Disconnected from userid:"* ]]; then
 
             # Extract steamid and reason
             steamid=$(echo "$line" | awk -F'[ :]+' '{print $4}')
@@ -37,8 +34,7 @@ LogParser() {
             SendDiscordMessage "$DISCORD_PLAYER_LEAVE_TITLE" "$message" "$DISCORD_PLAYER_LEAVE_COLOR"
         fi
 
-        if [[ "$line" == "Started session with Game ID "* ]]; then
-            [[ "${DISCORD_SERVER_START_ENABLED,,}" == false ]] && return 0
+        if [[ "${DISCORD_SERVER_START_ENABLED,,}" == true ]] && [[ "$line" == "Started session with Game ID "* ]]; then
 
             # Extract Game ID
             gameid=$(echo "$line" | awk '{print $6}')
